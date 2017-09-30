bbf0bcf49f18d329df1f7a279554e726199c852e
C-Beck <C-Beck@users.noreply.github.com>
2017-09-30 04:41:19 -0400

    Working on serial
    Need to send data in the form of 0XRXXGXXBXX with leading zero because
    it's to late to keep trying to fix the serial data bursts

--- Arduino/Servo_test/Servo_test.ino
+++ Arduino/Servo_test/Servo_test.ino
@@ -1,9 +1,41 @@
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+
 void setup() {
   // put your setup code here, to run once:
-
+  servo1.attach(2);
+  servo2.attach(3);
+  pinMode(52,OUTPUT);
+  digitalWrite(52,HIGH);
+  pinMode(53,OUTPUT);
+  digitalWrite(53,HIGH);
+  Serial.begin(57600);
 }
-
+int incomingBytes[11];
 void loop() {
   // put your main code here, to run repeatedly:
+  // have to send 0XRXXGXXBXX
+  // include leading zero as stopgap
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingBytes[0] = Serial.read();
+    incomingBytes[1] = Serial.read();
+    incomingBytes[2] = Serial.read();
+    incomingBytes[3] = Serial.read();
+    incomingBytes[4] = Serial.read();
+    incomingBytes[5] = Serial.read();
+    incomingBytes[6] = Serial.read();
+    incomingBytes[7] = Serial.read();
+    incomingBytes[8] = Serial.read();
+    incomingBytes[9] = Serial.read();
+    incomingBytes[10] = Serial.read();
 
+    // say what you got:
+    for(int x = 0; x < 11; x++){
+      Serial.print("I received: ");
+      Serial.println(char(incomingBytes[x]));
+    }
+  }
 }
